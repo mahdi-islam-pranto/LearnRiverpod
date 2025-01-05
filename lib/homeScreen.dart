645945e5edba104ede7c6faa23e471bd56a24f01
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2025-01-05 12:21:16 +0600

    counter (=/-) with state provider

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -6,13 +6,20 @@ final helloProvider = Provider<String>((ref) {
   return "Hello, World!";
 });
 
+// state provider
+final counterProvider = StateProvider<int>((ref) {
+  return 0;
+});
+
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    print("build");
     // get the value from the provider
     final hello = ref.watch(helloProvider);
+    // final count = ref.watch(counterProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Screen'),
@@ -22,11 +29,34 @@ class HomeScreen extends ConsumerWidget {
           children: [
             Text(hello),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/second');
-              },
-              child: const Text('Go to Second Screen'),
+            // cunsumer widget to read the value from the provider
+            Consumer(builder: (context, ref, child) {
+              // get the value from the provider
+              // watch -> to get the value from the provider
+              // read -> to read the value from the provider and update the value
+              final count = ref.watch(counterProvider);
+              print("Text widget build");
+              return Text('Counter: ' + count.toString());
+            }),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // read the value from the provider and update the value
+                    ref.read(counterProvider.notifier).state--;
+                  },
+                  child: const Text('-'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // read the value from the provider and update the value
+                    ref.read(counterProvider.notifier).state++;
+                  },
+                  child: const Text('+'),
+                ),
+              ],
             ),
           ],
         ),
